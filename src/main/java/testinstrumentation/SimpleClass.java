ac77e552beb6a759bd02049dd3d73a9844dc5181
Mithil Hari <mithilh@uci.edu>
2020-02-19 11:09:39 -0800

    add simple class to test interceptor

---
+++ src/main/java/testinstrumentation/SimpleClass.java
@@ -0,0 +1,15 @@
+package testinstrumentation;
+
+public class SimpleClass {
+    public static String sayHello(String name) {
+        return "Hello " + name + ", you fool, I love youuu! " + joinTheJoyRide();
+    }
+
+    public static String joinTheJoyRide() {
+        return "C'mon join the joyrideee!";
+    }
+
+    public static String sayGoodBye() {
+        return "Goodbye to you, goodbye to broken hearts";
+    }
+}
