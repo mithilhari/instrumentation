84704eed2f1f51eb1a602521cc8507baeadb22e0
Mithil Hari <mithilh@uci.edu>
2020-02-20 11:53:03 -0800

    Main method for SimpleClass

--- src/main/java/testinstrumentation/SimpleClass.java
+++ src/main/java/testinstrumentation/SimpleClass.java
@@ -12,4 +12,8 @@ public class SimpleClass {
     public static String sayGoodBye() {
         return "Goodbye to you, goodbye to broken hearts";
     }
+
+    public static void main(String [] args) {
+        System.out.println("Running SimpleClass main");
+    }
 }
