c825e2888a0cd89b283c4ff9354136ccc46b25b6
Mithil Hari <mithilh@uci.edu>
2020-02-24 11:41:37 -0800

    Added Hello World Agent

--- src/main/java/testinstrumentation/DependencyInstrumentation.java
+++ src/main/java/testinstrumentation/DependencyInstrumentation.java
@@ -1,6 +1,10 @@
 package testinstrumentation;
 
+import java.lang.instrument.Instrumentation;
 // Build Dependency graph of bytecode
 // If method is part of JDK ignore
 public class DependencyInstrumentation {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("Hello World Agent works fine!");;
+    }
 }
